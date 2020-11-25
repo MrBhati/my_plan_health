01dcdcf5d87537d7f4f7bf29a45e324120c1d8eb
MrBhati <mrbhatisurendra@gmail.com>
2020-11-25 09:42:20 +0530

    add login form

--- lib/UI/Login.dart
+++ lib/UI/Login.dart
@@ -8,7 +8,6 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
-
   TextEditingController _mobileController;
 
   @override
@@ -17,105 +16,113 @@ class _LoginState extends State<Login> {
     super.initState();
     _mobileController = TextEditingController();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFf8fbf4),
-       body: SafeArea(
-         child: Column(
-           children: [
-             Container(
-               height: MediaQuery.of(context).size.height / 2,
-decoration: BoxDecoration(
-  color: Colors.white,
-      borderRadius: BorderRadius.only(
-        bottomLeft: Radius.circular(40),
-        bottomRight: Radius.circular(40)
-      )
-),
-              child: Image.asset("assets/login.png"),
-             ),
-       Align(alignment: Alignment.center,
-       child: Text("Login With"),),
-
-
-       TextFormField(
-            style: TextStyle(
-              fontWeight: FontWeight.w500,
-              letterSpacing: 1.0,
-            ),
-            controller: _mobileController,
-            readOnly: false,
-            obscureText: false,
-            decoration: InputDecoration(
-              focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                      color: Color(0xFF9cca57),
-                      width: 1.0)),
-              enabledBorder: OutlineInputBorder(
-                borderSide: BorderSide(
-                    color:  Color(0xFF9cca57), width: 1.0),
-              ),
-              border: OutlineInputBorder(
-                  borderSide: BorderSide(
-                      color:  Color(0xFF9cca57), width: 1.0)),
-              labelText: 'Enter Mobile Number',
-              //labelStyle: AppFontStyle.hint_text_style,
-              hintText: 'Enter Mobile Number',
-              //hintStyle: AppFontStyle.hint_text_style,
-             
-            ),
-            onChanged: (value) {
-            
-            },
-          ),
-
-     Container(
-                child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+      backgroundColor: Color(0xFFe8ffc6),
+      body: SafeArea(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          child: SingleChildScrollView(
+            child: Column(
               children: [
                 Container(
-                  height: 0.2,
-                  width: MediaQuery.of(context).size.width / 2 - 32,
-                  color: Colors.black
-                ),
-                Text(
-                  "  Or  ",
-                  style: TextStyle(fontSize: 15),
+                  height: MediaQuery.of(context).size.height / 2 - 15,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(40),
+                          bottomRight: Radius.circular(40))),
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(10, 20, 10, 8),
+                    child: Image.asset("assets/login.png"),
+                  ),
                 ),
                 Container(
-                  width: MediaQuery.of(context).size.width / 2 - 32,
-                  height: 0.2,
-                  color: Colors.black,
-                )
+                  height: MediaQuery.of(context).size.height / 2 -15,
+                  child: Column(children: [
+                     SizedBox(height: 30),
+                Align(
+                  alignment: Alignment.center,
+                  child: Text(
+                    "Login With",
+                    style: TextStyle(color: Colors.green, fontSize: 12,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 18),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(40, 10, 40, 10),
+                  child: Container(
+                    height: 45,
+                    color: Colors.green,
+                    alignment: Alignment.center,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.mobile_friendly),
+                        SizedBox(width: 20),
+                        Text("Phone Number")
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(height: 30),
+                Container(
+                    child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                        height: 0.2,
+                        width: MediaQuery.of(context).size.width / 2 - 32,
+                        color: Colors.black),
+                    Text(
+                      "  Or  ",
+                      style: TextStyle(fontSize: 15),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width / 2 - 32,
+                      height: 0.2,
+                      color: Colors.black,
+                    )
+                  ],
+                )),
+                SizedBox(height: 30),
+                Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(width:20),
+                      Image(
+                          height: 60,
+                          width: 60,
+                          image: AssetImage('assets/fb.png')),
+                          SizedBox(width:25),
+                      Container(
+                        height: 30,
+                        width: 1,
+                        color: Colors.black,
+                      ),
+                      SizedBox(width:20),
+                      Image(
+                          height: 60,
+                          width: 60,
+                          image: AssetImage('assets/google.png')),
+                      SizedBox(width: 25),
+                    ],
+                  ),
+                ),
+                Expanded(child: Container()),
+                Text("By continuing, you agree to terms & Conditions")
+                  ],),
+                ),
+               
               ],
-            )),
-Container(
-  child:      Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-
-
-          
-                Image.asset("assets/fb.png"),
-           Container(
-             height: 50,
-             width: 1,
-             color: Colors.black,
-           ),
-            Image(
-                height: 60,
-                width: 60,
-                image: AssetImage('assets/google.png')),
-            SizedBox(width: 25),
-         
-          ],
+            ),
+          ),
         ),
-
-)
-           ],
-         ),
-       ),
+      ),
     );
   }
 }
---
+++ lib/UI/MobileNumber.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MobileNumber extends StatefulWidget {
+  MobileNumber({Key key}) : super(key: key);
+
+  @override
+  _MobileNumberState createState() => _MobileNumberState();
+}
+
+class _MobileNumberState extends State<MobileNumber> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: child,
+    );
+  }
+}
